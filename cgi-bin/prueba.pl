0464a4b18d557ecc7b55458d3dfaa748f02fe2a4
Cristhian <cristhian.bravo.8657@gmail.com>
2024-12-07 21:31:09 -0500

    Bibliotecadb - ejecucion manual

--- cgi-bin/prueba.pl
+++ cgi-bin/prueba.pl
@@ -5,28 +5,33 @@ use DBI;
 
 # Configuración de conexión
 my $database = "biblioteca";
-my $hostname = "biblioteca-db";              
+my $hostname = "bibliotecadb";              
 my $port     = 3306;   
-my $username = "Admin";  
-my $password = "password";   
+my $username = "root";  
+my $password = "rootpassword";   
 
 # DSN de conexión
 my $dsn = "DBI:mysql:database=$database;host=$hostname;port=$port";
 
-# Conectar a la base de datos
+# Conectar a la base de datos con manejo adecuado de errores
 my $dbh = DBI->connect($dsn, $username, $password, {
-    RaiseError       => 1,
-    PrintError       => 0,
-    mysql_enable_utf8 => 1,
-});
+    RaiseError       => 1,        # Lanza excepciones si ocurre un error
+    PrintError       => 1,        # Imprime los errores por consola
+    mysql_enable_utf8 => 1,       # Habilita UTF-8
+}) or die "No se pudo conectar a la base de datos: $DBI::errstr\n";
 
-# Validar conexión
-if ($dbh) {
-    print "Content-type: text/html\n\n";  
-} else {
-    die "Content-type: text/html\n\nError al conectar a la base de datos: $DBI::errstr\n";
-}
+# Si la conexión es exitosa, muestra el tipo de contenido
+print "Content-type: text/html\n\n";  
 
-# Cerrar la conexión
-$sth->finish();
+# Aquí puedes realizar las consultas o manipulaciones de la base de datos, por ejemplo:
+# my $sth = $dbh->prepare("SELECT * FROM tabla");
+# $sth->execute();
+
+# Si realizas una consulta o algún procesamiento, asegúrate de manejar errores si algo falla.
+# Si necesitas mostrar resultados, por ejemplo:
+# while (my $row = $sth->fetchrow_hashref) {
+#     print "$row->{columna}\n";
+# }
+
+# Cerrar la conexión correctamente
 $dbh->disconnect();
